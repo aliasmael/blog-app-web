25d9100e5a143070379b32c161d8b8c4bd081bc9
aliasmael <alilion22@ymail.com>
2018-01-27 16:19:10 +0200

    Add response time for calling mock service

--- src/components/Stream/service/Mock.ts
+++ src/components/Stream/service/Mock.ts
@@ -3,10 +3,14 @@ import { Blog } from '../models/Models'
 import blogs from '../fixtures/blogs'
 
 class Mock implements IService {
+  responseTime: number = 1000 //Response time in millisecond
+
   getStream(): Promise<Blog[]> {
     return new Promise((resolve, _) => {
-      let response: Blog[] = JSON.parse(JSON.stringify(blogs))
-      return resolve(response)
+      setTimeout(() => {
+        let response: Blog[] = JSON.parse(JSON.stringify(blogs))
+        return resolve(response)
+      }, this.responseTime)
     })
   }
 }
