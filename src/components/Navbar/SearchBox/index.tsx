9b377d8605c7ecaf8b997d3c1ed4af33023b27b5
aliasmael <alilion22@ymail.com>
2018-01-27 20:33:16 +0200

    Restyling search box

--- src/components/Navbar/SearchBox/index.tsx
+++ src/components/Navbar/SearchBox/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { Input } from 'antd'
-const Search = Input.Search;
+import style from './style'
+
+const Search = Input.Search
 
 export interface ISearchBoxProps {
 
@@ -12,7 +14,7 @@ export default class SearchBox extends React.Component<ISearchBoxProps> {
 	render() {
 
 		return (
-			<Search style={{height: "40px"}} placeholder="search for blogs" enterButton="Search" size="large" />
+			<Search style={style.searchBtn} placeholder="search for blogs" size='large' />
 		);
 	}
 
---
+++ src/components/Navbar/SearchBox/style.ts
@@ -0,0 +1,9 @@
+import { CSSProperties } from 'react'
+
+const style = {
+  searchBtn: {
+    height: 40
+  } as CSSProperties
+}
+
+export default style
